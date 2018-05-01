7424762ee955d69678664ae9872dc4ce3be9d785
vwheezy22 <vwhiz22@gmail.com>
2018-05-01 11:51:26 -0700

    axios

---
+++ Router/ConnectingToAPI/5.1.A/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue';
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    'todo-list': Todos
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Router/ConnectingToAPI/5.1.A/src/components/Todos.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+  	<h2> This is the Todo List</h2>
+  	<ul>
+  		<li v-for="todo in todos">{{ todo.title }}</li>
+  	</ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data () {
+    return {
+    	todos: []
+    }
+  },
+  created () {		// uses created lifecycle hook, runs after component is created
+  		console.log('created called');
+  		this.showTodos();
+  },
+  methods: {
+  	showTodos (param) {
+  		console.log('showTodos called');
+  		let vm = this;
+  		axios.get('https://jsonplaceholder.typicode.com/posts')	// gets data from api
+  		.then(function(response) {
+  			vm.todos = response.data.slice(0,10);	// gets first ten entries in data and stores into todos data
+  			console.log(vm.todos);
+  		})
+  		.catch(function(err) {
+  			console.log(err);
+  		})
+  	}
+  }
+}
+</script>
+
+<style>
+
+</style>
